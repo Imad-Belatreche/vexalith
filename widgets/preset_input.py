9650b04ff9c0dc54e1b94244db1bd8bceb329d94
Imad-Belatreche <imad.belatreche@etu.usthb.dz>
2026-03-26 14:50:33 +0100

    Refs #26: Add debounce mode to speake after x time
    - "ctrl+t" to change the speaking mode
    - made sure to only speak unspoken text
    - default timer is 0.8s

--- widgets/preset_input.py
+++ widgets/preset_input.py
@@ -1,7 +1,7 @@
 from queue import Queue
 from textual import on
 from textual.widgets import Input, ListItem, ListView, RichLog
-from constants import CONFIG_FILE
+from constants import CONFIG_FILE, SpeakModes
 from utils import save_config
 from widgets.label_item import LabelItem
 
@@ -24,11 +24,30 @@ class PresetInput(Input):
     index = 0
     old_val = ""
     used_history = False
+
+    last_spoken = ""
+    debounce_timer = None
+    mode = SpeakModes.SUBMIT
+
     BINDINGS = [
+        ("ctrl+t", "change_mode", "Change mode"),
         ("ctrl+o", "save_preset", "Save preset"),
         ("up", "history_up", "History up"),
         ("down", "history_down", "History down"),
     ]
+
+    def speak_only_unspoken(self, current_text: str):
+        if self.last_spoken.startswith(current_text):
+            pass
+        elif current_text.startswith(self.last_spoken):
+            new_text = current_text[len(self.last_spoken) :].strip()
+            if new_text:
+                self.audio_queue.put(new_text)
+        else:
+            if current_text.strip():
+                self.audio_queue.put(current_text)
+
+        self.last_spoken = current_text
 
     def on_mount(self):
         self.index = len(self.history)
@@ -54,6 +73,7 @@ class PresetInput(Input):
             self.value = self.old_val
             self.used_history = False
             self.cursor_position = len(self.value)
+            self.last_spoken = ""
 
     def action_save_preset(self):
         print(f"Configs type save preset: {type(self.configs)}")
@@ -67,10 +87,33 @@ class PresetInput(Input):
                 ListItem(LabelItem(preset_text))
             )
 
+    def action_change_mode(self):
+        if self.mode == SpeakModes.SUBMIT:
+            self.mode = SpeakModes.DEBOUNCE
+        else:
+            self.mode = SpeakModes.SUBMIT
+
+        self.app.query_one(RichLog).write(f"[SYSTEM] Mode changed to: {self.mode.name}")
+
     @on(Input.Changed)
     def save_old_value(self, event: Input.Changed):
         if not self.used_history:
-            self.old_val = event.input.value
+            self.old_val = event.input.value.strip()
+
+        if self.mode == SpeakModes.DEBOUNCE:
+            if self.debounce_timer is not None:
+                self.debounce_timer.stop()
+
+            text_speak = event.value
+
+            if text_speak and not self.used_history:
+                current_debounce = float(
+                    self.configs.get("settings", {}).get("debounce_time", 0.8)
+                )
+                self.debounce_timer = self.set_timer(
+                    current_debounce,
+                    lambda: self.speak_only_unspoken(text_speak),
+                )
 
     @on(Input.Submitted)
     def handle_input_submition(self, event: Input.Submitted):
@@ -80,12 +123,16 @@ class PresetInput(Input):
         if input_text == self.history[-1] if self.history else None:
             self.value = ""
             self.index = len(self.history)
-            self.audio_queue.put(input_text)
+            if self.mode == SpeakModes.SUBMIT:
+                self.audio_queue.put(input_text)
+            self.last_spoken = ""
             return
-        self.audio_queue.put(input_text)
+        if self.mode == SpeakModes.SUBMIT:
+            self.audio_queue.put(input_text)
         logs = self.app.query_one(RichLog)
         logs.write(f"- {input_text}")
         event.input.value = ""
         self.history.append(input_text)
         self.index = len(self.history)
         self.used_history = False
+        self.last_spoken = ""
